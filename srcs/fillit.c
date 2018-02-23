4fefcf83542a34f6a154d339da6e8bd6848d515a
Rafael Zarate <rafazrte@gmail.com>
2018-02-23 02:51:28 -0800

    Adding a batch of files

--- srcs/fillit.c
+++ srcs/fillit.c
@@ -1,25 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fillit.h                                           :+:      :+:    :+:   */
+/*   fillit.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/23 01:48:59 by rzarate           #+#    #+#             */
-/*   Updated: 2018/02/23 02:50:41 by rzarate          ###   ########.fr       */
+/*   Created: 2018/02/23 02:48:46 by rzarate           #+#    #+#             */
+/*   Updated: 2018/02/23 02:50:00 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLIT_H
-# define FILLIT_H
+#include "../includes/fillit.h"
 
-#define BUF_SIZE 4096
+void	fillit(char	*s)
+{
+	int i;
 
-#include "../srcs/libft/libft.h"
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-
-void	fillit(char	*s);
-
-#endif
+	i = 0;
+}
--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,38 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 01:16:09 by rzarate           #+#    #+#             */
-/*   Updated: 2018/02/23 01:41:16 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/02/23 02:50:46 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fillit.h"
+#include "../includes/fillit.h"
 
-int	main(int ag,char  **av)
+int	main(int ac,char  **av)
 {
+	int	fd;
+	int	ret;
+	char	buf[BUF_SIZE + 1];
 
+	if (!ac || ac > 1)
+	{
+		ft_putstr_fd(2, "you dumb piece of shittttt");
+	}
+	else if (ac == 1)
+	{
+		fd = open(av[0], O_RDONLY);
+		if (fd == -1)
+		{
+			ft_putstr_fd(2, "Couldn't open file");
+			return (0);
+		}
+		ret = read(fd, buf, BUF_SIZE);
+		ret = '\0';
+		fillit(ret);
+		if (close(fd) == -1)
+		{
+			ft_pustr_fd(2, "Couldn't close file");
+			return (0);
+		}
+	}
 	return (0);
 }
