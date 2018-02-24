f2aa7d9e64f51c648b6efb2cd0ae32b288f87c80
Rafael Zarate <rafazrte@gmail.com>
2018-02-24 08:41:15 -0800

    Submitting changes

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 01:48:59 by rzarate           #+#    #+#             */
-/*   Updated: 2018/02/24 03:53:33 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/02/24 04:57:40 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void			fillit(char	*s);
 int				check_input(char *s);
 int			*make_tets(int	**at);
 int			**parse_tets(char *s);
+int			count_tets(char *s);
 void		solve_tetrimino(int	**p);
 
 #endif
--- srcs/fillit.c
+++ srcs/fillit.c
@@ -6,7 +6,7 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 02:48:46 by rzarate           #+#    #+#             */
-/*   Updated: 2018/02/24 04:17:42 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/02/24 07:09:53 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,18 @@
 
 void	fillit(char	*s)
 {
-	int **p;
-	int i;
-	int y;
+	int n;
 
-	i = -1;
-	y = -1;
-	if (check_input(s) == -1)
+	n = check_input(s);
+	if (n == -1)
 	{
 		ft_putendl_fd("File is blank", 2);
 		return ;
 	}
-	else if (check_input(s) == 0)
+	else if (n == 0)
 	{
 		ft_putendl_fd("Usage text goes here", 2);
 		return ;
 	}
-	else
-		p = parse_tets(s);
-		
+	return ;	
 }
